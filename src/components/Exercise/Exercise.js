e261bf3cb5a955b45a755c3276365ce2ac9f6026
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-09-29 03:31:21 +0600

    json data modified, images added

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const Exercise = ({ exercise }) => {
     const { picture, name, age, about, time } = exercise;
+    console.log(picture)
+
+    let slicedTxt;
+    if (about.length > 100) {
+        slicedTxt = about.slice(0, 100) + '...';
+    }
+    else
+        slicedTxt = about;
+
     return (
         <div>
             <div className='bg-gray-50'>
-                <img src={picture} alt="fitness" />
+                <img className='object-cover h-64' src={picture} alt="fitness" />
                 <h1 className="text-2xl font-semibold mt-2 mx-3">{name}</h1>
-                <p className=" text-slate-600 mx-3 my-2">{about}</p>
+                <p className=" text-slate-600 mx-3 my-2">{slicedTxt}</p>
                 <p className="font-semibold mx-3">For Age: {age}</p>
                 <p className="font-semibold mx-3">Time required: {time}</p>
                 <div className='text-center py-5'>
